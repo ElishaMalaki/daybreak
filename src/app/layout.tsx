11cc2056cfdf50f3605392bbaa50c63168c80e87
ElishaMalaki <144005119+ElishaMalaki@users.noreply.github.com>
2026-09-23 12:45:26 +0300

    security: remove global third-party runtime scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,12 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
-
-        <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Fdaybreak1966back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.20" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.3" /></body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
